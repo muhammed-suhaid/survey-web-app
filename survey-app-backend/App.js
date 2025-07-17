49b848c28720eb03ec223833998c83ddf91adc6b
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-07-17 08:24:44 +0530

    Backend Completed

--- survey-app-backend/App.js
+++ survey-app-backend/App.js
@@ -1,13 +1,32 @@
-const express =require("express")
-const mongoose =require("mongoose")
-const cors =require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 
-const app=express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.listen(4000,()=>{
+app.post("/survey", (request, response) => {
+    const getName = (request.body.name)
+    const getGender = (request.body.gender)
+    const getBloodGrp = (request.body.blood_group)
+    const getPinCode = (request.body.pin_code)
+    const getMobileNo = (request.body.mobile_no)
+
+    response.json({
+        "status": "Success",
+        "result": {
+            "name": getName,
+            "gender": getGender,
+            "blood_group": getBloodGrp,
+            "pin_code": getPinCode,
+            "mobile_no": getMobileNo,
+        }
+    })
+})
+
+app.listen(4000, () => {
     console.log("Srever is Running <3")
 })
