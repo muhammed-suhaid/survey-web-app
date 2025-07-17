e32ded30fb7675991efa4fe6fc6b63a1612b8280
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-07-17 09:46:28 +0530

    Frontend COmpleted

---
+++ survey-app-frontend/src/components/AddDetails.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import NavBar from './NavBar'
+
+const AddDetails = () => {
+    const [input, changeInput] = useState(
+        {
+            "name": "",
+            "gender": "",
+            "blood_group": "",
+            "pin_code": "",
+            "mobile_no": ""
+        }
+    )
+
+    const [isData, changeIsData] = useState(false)
+
+    const [output, changeOutput] = useState(
+        {
+            "status": "",
+            "result": {
+                "name": "",
+                "gender": "",
+                "blood_group": "",
+                "pin_code": "",
+                "mobile_no": ""
+            }
+        }
+    )
+
+    const inputHandler = (event) => {
+        changeInput({ ...input, [event.target.name]: event.target.value })
+    }
+
+    const onButtonClick = () => {
+        axios.post("http://Localhost:4000/survey", input)
+            .then((response) => {
+                changeOutput(response.data.result)
+                changeIsData(true)
+            })
+            .catch(() => { console.log("Something went Wrong!") })
+    }
+
+    return (
+        <div>
+            <NavBar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3 p-5">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Name</label>
+                                <input type="text" className="form-control" name="name" value={input.name} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label className="form-label d-block">Gender</label>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gender" id="male" value="Male" checked={input.gender === "Male"} onChange={inputHandler} />
+                                    <label className="form-check-label" htmlFor="male">Male</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gender" id="female" value="Female" checked={input.gender === "Female"} onChange={inputHandler} />
+                                    <label className="form-check-label" htmlFor="female">Female</label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gender" id="other" value="Other" checked={input.gender === "Other"} onChange={inputHandler} />
+                                    <label className="form-check-label" htmlFor="other">Other</label>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Blood Group</label>
+                                <input type="text" className="form-control" name="blood_group" value={input.blood_group} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Pin Code</label>
+                                <input type="text" className="form-control" name="pin_code" value={input.pin_code} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Mobile Number</label>
+                                <input type="text" className="form-control" name="mobile_no" value={input.mobile_no} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-success" type="button" onClick={onButtonClick}>SUBMIT</button>
+                                </div>
+                            </div>
+                            {/* Display Card */}
+                            {isData ? <div className="mt-4 col-12 col-md-6 mx-auto">
+                                <div className="card shadow">
+                                    <div className="card-body">
+                                        <h5 className="card-title">Survey Result</h5>
+                                        <p className="card-text"><strong>Name:</strong> {output.name}</p>
+                                        <p className="card-text"><strong>Gender:</strong> {output.gender}</p>
+                                        <p className="card-text"><strong>Blood Group:</strong> {output.blood_group}</p>
+                                        <p className="card-text"><strong>Pin Code:</strong> {output.pin_code}</p>
+                                        <p className="card-text"><strong>Mobile Number:</strong> {output.mobile_no}</p>
+                                    </div>
+                                </div>
+                            </div> : <p></p>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddDetails
---
+++ survey-app-frontend/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-info">
+                <div class="container-fluid">
+                    <Link class="navbar-brand active" aria-current="page" to="/">Internal Mark Calculator</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
