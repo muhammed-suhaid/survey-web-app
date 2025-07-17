37e9f0ce088363ba674d0f006ba4daacdb3067a6
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-07-17 10:04:39 +0530

    working clean

--- survey-app-frontend/src/components/NavBar.jsx
+++ survey-app-frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div>
             <nav class="navbar navbar-expand-lg bg-info">
                 <div class="container-fluid">
-                    <Link class="navbar-brand active" aria-current="page" to="/">Internal Mark Calculator</Link>
+                    <Link class="navbar-brand active" aria-current="page" to="/">Survey App</Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
